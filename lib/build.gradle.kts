27e94185e54195aca1b9a7e96b74b45473d5fe03
Shreck Ye <ShreckYe@gmail.com>
2022-07-27 20:37:42 +0800

    Update the Kotlin version to 1.7.10

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation(commonDependencies.vertx.moduleWithoutVersion("pg-client"))
 }
 
-version = "0.1.1-kotlin-1.6.10-SNAPSHOT"
+version = "0.1.1-SNAPSHOT"
 
 publishing.publications.getByName<MavenPublication>("maven") {
     artifactId = rootProject.name + "-postgresql"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
+        classpath(kotlin("gradle-plugin", "1.7.10"))
         classpath("com.huanshankeji:kotlin-common-gradle-plugins:0.1.7-kotlin-1.6.10")
         classpath("com.huanshankeji:common-gradle-dependencies:0.2.0-20220720-kotlin-1.6.10")
     }
