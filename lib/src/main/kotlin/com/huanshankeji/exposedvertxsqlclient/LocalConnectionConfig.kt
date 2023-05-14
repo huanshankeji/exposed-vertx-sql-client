91d770011718211d08f07be4eef9a27039f2c58d
Shreck Ye <ShreckYe@gmail.com>
2023-05-15 03:01:13 +0800

    Add a TODO

--- lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/LocalConnectionConfig.kt
+++ lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/LocalConnectionConfig.kt
@@ -2,6 +2,7 @@ package com.huanshankeji.exposedvertxsqlclient
 
 import com.huanshankeji.net.LOCALHOST
 
+// TODO: move to a separate package and consider adding a prefix word such as "default" or "conventional" as this class is not general enough
 class LocalConnectionConfig(val database: String, val user: String, val socketConnectionPassword: String) {
     companion object {
         const val UNIX_DOMAIN_SOCKET_PATH = "/var/run/postgresql"
