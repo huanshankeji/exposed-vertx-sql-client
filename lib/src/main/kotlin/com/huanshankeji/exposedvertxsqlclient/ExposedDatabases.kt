adb89cee1489508f14eceaf3f31e778640d5f81a
Shreck Ye <ShreckYe@gmail.com>
2023-05-16 00:37:25 +0800

    Fix a bug in 404c7af4a0f89dd21ba96b718e7d9107a44f96f2

--- lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/ExposedDatabases.kt
+++ lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/ExposedDatabases.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.Database
 fun exposedDatabaseConnectPostgreSql(socketConnectionConfig: ConnectionConfig.Socket) =
     with(socketConnectionConfig) {
         Database.connect(
-            "jdbc:postgresql://$host${port?.let { ":it" } ?: ""}/$database",
+            "jdbc:postgresql://$host${port?.let { ":$it" } ?: ""}/$database",
             "org.postgresql.Driver",
             user = user,
             password = password
