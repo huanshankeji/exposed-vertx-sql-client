2a7066d465b91d06b793a04e121a8a9e8bff657f
Shreck Ye <ShreckYe@gmail.com>
2024-03-15 10:45:44 +0800

    v0.3.0 release

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -2,7 +2,7 @@ import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonGradleClasspathDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.3.0-SNAPSHOT"
+val projectVersion = "0.3.0"
 
 val commonVersions = CommonVersions()
 val commonDependencies = CommonDependencies(commonVersions)
