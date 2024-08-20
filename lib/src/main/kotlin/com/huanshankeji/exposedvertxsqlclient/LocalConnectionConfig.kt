c82aa75bbe8c8d6254cbcfcc6dbce2e94d2c84ca
Shreck Ye <ShreckYe@gmail.com>
2024-08-21 04:24:17 +0800

    Add a KDoc

--- lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/LocalConnectionConfig.kt
+++ lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/LocalConnectionConfig.kt
@@ -3,6 +3,10 @@ package com.huanshankeji.exposedvertxsqlclient
 import com.huanshankeji.net.LOCALHOST
 
 // TODO: move to a separate package and consider adding a prefix word such as "default" or "conventional" as this class is not general enough
+/**
+ * A kind of connection config that can produce both a [ConnectionConfig.Socket] and a [ConnectionConfig.UnixDomainSocketWithPeerAuthentication]
+ * to connect to a local database server.
+ */
 class LocalConnectionConfig(val database: String, val user: String, val socketConnectionPassword: String) {
     companion object {
         const val UNIX_DOMAIN_SOCKET_PATH = "/var/run/postgresql"
