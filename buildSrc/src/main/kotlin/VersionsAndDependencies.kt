15f7e1cfe0afe78201ca146074d5027d93a87fac
Shreck Ye <ShreckYe@gmail.com>
2024-08-22 07:47:04 +0800

    Bump the project version to "0.4.0-SNAPSHOT"

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -2,7 +2,7 @@ import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonGradleClasspathDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.3.0-SNAPSHOT"
+val projectVersion = "0.4.0-SNAPSHOT"
 
 val commonVersions = CommonVersions()
 val commonDependencies = CommonDependencies(commonVersions)
