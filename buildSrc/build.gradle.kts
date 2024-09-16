c649756a441c3edcb33c1275add0a056a760eb56
Shreck Ye <ShreckYe@gmail.com>
2024-09-16 11:12:22 +0800

    Debug with an internal project and bump Exposed to 0.53.0 with the latest snapshot version of "common-gradle-dependencies"

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,6 +17,6 @@ repositories {
 dependencies {
     // With Kotlin 2.0.20, a "Could not parse POM" build error occurs in the JVM projects of some dependent projects.
     implementation(kotlin("gradle-plugin", "2.0.10"))
-    implementation("com.huanshankeji:common-gradle-dependencies:0.7.1-20240516")
+    implementation("com.huanshankeji:common-gradle-dependencies:0.7.2-20240916-SNAPSHOT") // TODO don't use a snapshot version in a main branch
     implementation("com.huanshankeji.team:gradle-plugins:0.5.1")
 }
