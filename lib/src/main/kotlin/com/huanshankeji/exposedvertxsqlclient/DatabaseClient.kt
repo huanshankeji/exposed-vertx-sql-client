5db3b675298a057d594fb4cf19fcb0e25762fc4c
Shreck Ye <ShreckYe@gmail.com>
2024-09-21 09:00:27 +0800

    Copy the 3 extension functions `FieldSet.getFieldExpressionSet`, `Query.getFieldExpressionSet`, `Row.toExposedResultRow` back into `DatabaseClient` and adapt them with `exposedTransaction` and update call sites correspondingly, to fix a bug that an Exposed transaction is required when calling these functions if the `FieldSet` contains custom functions depending on dialects (which requires transactions) and no such a transaction is provided
    There can be further optimization by calling `getFieldExpressionSet` inside existing transactions (the ones used to prepare the query). This is added as a TODO.
    
    The functions were moved out of `DatabaseClient` in commit 680e3ceb38b389b306ab24cf5663c5570330920c but now copied back in again and renamed.

--- lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/DatabaseClient.kt
+++ lib/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/DatabaseClient.kt
@@ -172,10 +172,24 @@ class DatabaseClient<out VertxSqlClient : SqlClient>(
     suspend fun <U> executeWithMapping(statement: Statement<*>, RowMapper: Function<Row, U>): RowSet<U> =
         execute(statement) { mapping(RowMapper) }
 
+    // TODO call `getFieldExpressionSet` inside existing transactions (the ones used to prepare the query) to further optimize the performance
+    @ExperimentalEvscApi
+    fun FieldSet.getFieldExpressionSetWithTransaction() =
+        exposedTransaction { getFieldExpressionSet() }
+
+    @Deprecated("This function is called nowhere except `Row.toExposedResultRowWithTransaction`. Consider inlining and removing it.")
+    @ExperimentalEvscApi
+    fun Query.getFieldExpressionSetWithTransaction() =
+        set.getFieldExpressionSetWithTransaction()
+
+    @ExperimentalEvscApi
+    fun Row.toExposedResultRowWithTransaction(query: Query) =
+        toExposedResultRow(query.getFieldExpressionSetWithTransaction())
+
     suspend inline fun <Data> executeQuery(
         query: Query, crossinline resultRowMapper: ResultRow.() -> Data
     ): RowSet<Data> =
-        executeWithMapping(query) { row -> row.toExposedResultRow(query).resultRowMapper() }
+        executeWithMapping(query) { row -> row.toExposedResultRowWithTransaction(query).resultRowMapper() }
 
     suspend fun executeQuery(query: Query): RowSet<ResultRow> =
         executeQuery(query) { this }
@@ -280,7 +294,7 @@ class DatabaseClient<out VertxSqlClient : SqlClient>(
     suspend inline fun <Data> executeBatchQuery(
         fieldSet: FieldSet, queries: Iterable<Query>, crossinline resultRowMapper: ResultRow.() -> Data
     ): Sequence<RowSet<Data>> {
-        val fieldExpressionSet = fieldSet.getFieldExpressionSet()
+        val fieldExpressionSet = fieldSet.getFieldExpressionSetWithTransaction()
         return executeBatch(queries) {
             mapping { row -> row.toExposedResultRow(fieldExpressionSet).resultRowMapper() }
         }
@@ -329,13 +343,29 @@ fun Row.toExposedResultRow(fieldExpressionSet: Set<Expression<*>>) =
         }.toMap()
     )
 
+private const val USE_THE_ONE_IN_DATABASE_CLIENT_BECAUSE_TRANSACTION_REQUIRED_MESSAGE =
+    "Use the one in `DatabaseClient` because a transaction may be required."
+
+/**
+ * An Exposed transaction is required if the [FieldSet] contains custom functions that depend on dialects.
+ */
+//@Deprecated(USE_THE_ONE_IN_DATABASE_CLIENT_BECAUSE_TRANSACTION_REQUIRED_MESSAGE)
 fun FieldSet.getFieldExpressionSet() =
     /** [org.jetbrains.exposed.sql.AbstractQuery.ResultIterator.fieldsIndex] */
     realFields.toSet()
 
+/**
+ * @see FieldSet.getFieldExpressionSet
+ */
+@Deprecated("This function is called nowhere except `Row.toExposedResultRow`. Consider inlining and removing it.")
+//@Deprecated(USE_THE_ONE_IN_DATABASE_CLIENT_BECAUSE_TRANSACTION_REQUIRED_MESSAGE)
 fun Query.getFieldExpressionSet() =
     set.getFieldExpressionSet()
 
+/**
+ * @see FieldSet.getFieldExpressionSet
+ */
+//@Deprecated(USE_THE_ONE_IN_DATABASE_CLIENT_BECAUSE_TRANSACTION_REQUIRED_MESSAGE)
 fun Row.toExposedResultRow(query: Query) =
     toExposedResultRow(query.getFieldExpressionSet())
 
