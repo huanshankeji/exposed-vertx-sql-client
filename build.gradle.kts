16aa23182656c7082297270361475648e003b28e
Shreck Ye <ShreckYe@gmail.com>
2024-11-06 10:09:23 +0800

    Configure the Dokka Gradle plugin, its tasks, and the GitHub Actions workflow, with code copied and adapted from "exposed-adt-mapping"
    A source commit: https://github.com/huanshankeji/exposed-adt-mapping/commit/18800b706e5d5bacd23ca9e20d8389bbef7d2a02

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
+
+plugins {
+    id("org.jetbrains.dokka")
+}
+
+dependencies {
+    dokka(project(":exposed-vertx-sql-client-postgresql"))
+}
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,4 +19,5 @@ dependencies {
     implementation(kotlin("gradle-plugin", "2.0.10"))
     implementation("com.huanshankeji:common-gradle-dependencies:0.8.0-20241016") // don't use a snapshot version in a main branch
     implementation("com.huanshankeji.team:gradle-plugins:0.6.0") // don't use a snapshot version in a main branch
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.0.0-Beta")
 }
