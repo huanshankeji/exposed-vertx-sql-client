d4773cdedc70ca990dd243326ac22dab018e33e6
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-20 01:52:50 +0800

    Remove the factory interfaces which are not needed any more with the current API design

--- core/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/exposed/ExposedDatabaseFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.huanshankeji.exposedvertxsqlclient.exposed
-
-import org.jetbrains.exposed.sql.Database
-
-// TODO remove
-interface ExposedDatabaseFactory {
-    fun databaseConnect(): Database
-}
--- core/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/vertx/sqlclient/VertxSqlClientFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.huanshankeji.exposedvertxsqlclient.vertx.sqlclient
-
-import io.vertx.sqlclient.SqlClient
-
-// TODO remove
-interface VertxSqlClientFactory<SqlClientT : SqlClient> {
-    fun buildSqlClient(): SqlClientT
-}
