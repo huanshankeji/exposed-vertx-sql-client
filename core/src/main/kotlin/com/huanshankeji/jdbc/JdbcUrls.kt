ef6d3d1f581da697030e4f8c9ce2d8a40a6034c1
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-20 02:35:04 +0800

    Move some code to https://github.com/huanshankeji/kotlin-common
    The corresponding commit: https://github.com/huanshankeji/kotlin-common/commit/1b93c185e51b4cdb3368d2813484f62022e518d9

--- core/src/main/kotlin/com/huanshankeji/jdbc/JdbcUrls.kt
+++
@@ -1,6 +0,0 @@
-// TODO move to "kotlin-common"
-
-package com.huanshankeji.jdbc
-
-fun jdbcUrl(rdbms: String, host: String, port: Int?, database: String) =
-    "jdbc:$rdbms://$host${port?.let { ":$it" } ?: ""}/$database"
