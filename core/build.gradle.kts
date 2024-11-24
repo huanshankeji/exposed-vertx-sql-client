5cf8b81992b18e7950c782e6b8e26f326453c0b0
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-24 14:01:45 +0800

    Move a TODO and the "exposed-jdbc" dependency to the "postgresql" module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
 
 dependencies {
     api(commonDependencies.exposed.core()) // TODO: use `implementation` when possible
-    // TODO: remove the Exposed JDBC dependency and the PostgresSQL dependency when there is no need to to generate SQLs with an Exposed transaction
-    runtimeOnly(commonDependencies.exposed.module("jdbc"))
     api(commonDependencies.kotlinCommon.exposed())
 
     with(commonDependencies.vertx) {
