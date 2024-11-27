0c5fe28a6956edd7bb3a163ad40cdf5c1b472eec
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-27 12:53:51 +0800

    Comment out a dependency that's no longer needed

--- postgresql/build.gradle.kts
+++ postgresql/build.gradle.kts
@@ -13,5 +13,5 @@ dependencies {
     runtimeOnly(commonDependencies.postgreSql())
     implementation(commonDependencies.vertx.moduleWithoutVersion("pg-client"))
     implementation(commonDependencies.kotlinCommon.core()) // for `Untested`
-    implementation(commonDependencies.kotlinCommon.vertx()) // for `PgPoolOptions.setUpConventionally`
+    //implementation(commonDependencies.kotlinCommon.vertx()) // for `PgPoolOptions.setUpConventionally`
 }
