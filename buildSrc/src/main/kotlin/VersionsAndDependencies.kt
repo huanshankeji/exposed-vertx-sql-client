a6ead337f0e89164371e40234285789c14aa5918
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-29 22:33:46 +0800

    Bump the snapshot dependencies to the stable versions

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -4,11 +4,11 @@ import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.5.0-SNAPSHOT"
 
-// TODO don't use a snapshot version in a main branch
-val commonVersions = CommonVersions(kotlinCommon = "0.6.0-SNAPSHOT", exposed = "0.56.0")
+// don't use a snapshot version in a main branch
+val commonVersions = CommonVersions(kotlinCommon = "0.6.0", exposed = "0.56.0")
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
 
 object DependencyVersions {
-    val exposedAdtMapping = "0.3.0-SNAPSHOT" // TODO don't use a snapshot version in a main branch
+    val exposedAdtMapping = "0.3.0" // don't use a snapshot version in a main branch
 }
