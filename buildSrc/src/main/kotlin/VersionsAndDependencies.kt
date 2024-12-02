05cffb1031cdbe929fd7abf291511cc1af845774
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-02 17:51:03 +0800

    Make the project version explicit

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -2,7 +2,7 @@ import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonGradleClasspathDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.5.1-SNAPSHOT"
+val projectVersion = "0.5.1-kotlin-2.1.0-SNAPSHOT"
 
 // don't use a snapshot version in a main branch
 val commonVersions = CommonVersions(kotlinCommon = "0.6.0", exposed = "0.56.0")
