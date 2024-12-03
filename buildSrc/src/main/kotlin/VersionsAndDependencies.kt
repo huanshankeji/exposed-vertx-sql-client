8732906d4bfd58c3586a5ed4c259673a544c7992
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-04 01:41:42 +0800

    Bump our Gradle plugins to the latest stable version v0.8.0 and remove the explicit "kotlin-2.1.0" in the library project versions

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -2,13 +2,13 @@ import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonGradleClasspathDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.5.1-kotlin-2.1.0-SNAPSHOT"
+val projectVersion = "0.5.1-SNAPSHOT"
 
 // TODO don't use a snapshot version in a main branch
-val commonVersions = CommonVersions(kotlinCommon = "0.6.1-kotlin-2.1.0-SNAPSHOT", exposed = "0.56.0")
+val commonVersions = CommonVersions(kotlinCommon = "0.6.1-SNAPSHOT", exposed = "0.56.0")
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
 
 object DependencyVersions {
-    val exposedAdtMapping = "0.3.1-kotlin-2.1.0-SNAPSHOT" // TODO don't use a snapshot version in a main branch
+    val exposedAdtMapping = "0.3.1-SNAPSHOT" // TODO don't use a snapshot version in a main branch
 }
