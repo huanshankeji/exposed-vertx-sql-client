48cff78a4c76f47f2b3c3636865c01c33fc08eac
Alfred Xiang <117074807+waste-of-kindergarten@users.noreply.github.com>
2025-01-18 09:38:33 +0100

    Update mysql/build.gradle.kts
    Co-authored-by: Yongshun Shreck Ye <ShreckYe@gmail.com>

--- mysql/build.gradle.kts
+++ mysql/build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
 dependencies {
     with(commonDependencies.vertx) { implementation(platformStackDepchain()) } // needed
     implementation(cpnProject(project, ":core"))
-    implementation("com.mysql:mysql-connector-j:9.1.0")
-    // TODO remove the Exposed JDBC dependency and the PostgresSQL dependency when there is no need to to generate SQLs with an Exposed transaction
+    // TODO remove the Exposed JDBC dependency and the MySQL dependency when there is no need to to generate SQLs with an Exposed transaction
     runtimeOnly(commonDependencies.exposed.module("jdbc"))
+    implementation("com.mysql:mysql-connector-j:9.1.0")
     implementation(commonDependencies.vertx.moduleWithoutVersion("mysql-client"))
     implementation(commonDependencies.kotlinCommon.core()) // for `Untested`
     // implementation(commonDependencies.kotlinCommon.vertx()) // for `PgPoolOptions.setUpConventionally`
