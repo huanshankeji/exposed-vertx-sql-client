c9d6fef81a491688a4f205fcee2a1d8b5b617fca
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-03-15 17:25:03 +0800

    Resolve https://github.com/huanshankeji/exposed-vertx-sql-client/pull/18#discussion_r1921050999

--- mysql/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/mysql/DatabaseClient.kt
+++ mysql/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/mysql/DatabaseClient.kt
@@ -2,8 +2,8 @@ package com.huanshankeji.exposedvertxsqlclient.mysql
 
 import com.huanshankeji.exposedvertxsqlclient.DatabaseClient
 import com.huanshankeji.exposedvertxsqlclient.withTypedTransaction
+import io.vertx.mysqlclient.MySQLConnection
 import io.vertx.sqlclient.Pool
-import io.vertx.sqlclient.SqlConnection
 
-suspend fun <T> DatabaseClient<Pool>.withSQLTransaction(function: suspend (DatabaseClient<SqlConnection>) -> T): T =
+suspend fun <T> DatabaseClient<Pool>.withMySqlTransaction(function: suspend (DatabaseClient<MySQLConnection>) -> T): T =
     withTypedTransaction(function)
