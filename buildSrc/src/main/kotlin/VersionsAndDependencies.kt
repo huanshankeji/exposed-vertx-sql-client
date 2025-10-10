a20efe04b650a5fd1f30af65b34320dc1ad63dc8
Yongshun Ye <ShreckYe@gmail.com>
2025-10-11 00:24:23 +0800

    Set Vert.x's explicit version

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -6,7 +6,8 @@ val projectVersion = "0.6.0-SNAPSHOT"
 
 // TODO don't use a snapshot version in a main branch
 // TODO remove Exposed's explicit version when migration to Exposed 1.0.0 is complete
-val commonVersions = CommonVersions(kotlinCommon = "0.7.0-SNAPSHOT", exposed = "0.61.0")
+// TODO remove Vert.x's explicit version when migration to Vert.x 5 is complete
+val commonVersions = CommonVersions(kotlinCommon = "0.7.0-SNAPSHOT", exposed = "0.61.0", vertx = "4.5.21")
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
 
