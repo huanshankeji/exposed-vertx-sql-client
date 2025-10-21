937777008d4b2aea074a46f5b1de15e30cca0a8d
Yongshun Ye <ShreckYe@gmail.com>
2025-10-21 17:23:25 +0800

    Try removing the `kotlin-common-exposed` dependency and the project builds

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     api(commonDependencies.exposed.core()) // TODO: use `implementation` when possible
     api(commonDependencies.exposed.module("jdbc"))
-    api(commonDependencies.kotlinCommon.exposed())
+    //api(commonDependencies.kotlinCommon.exposed())
 
     with(commonDependencies.vertx) {
         implementation(platformStackDepchain())
