f8d11262d43e0baac26bdc51d43d316cd197f77e
Yongshun Ye <ShreckYe@gmail.com>
2025-10-28 02:59:39 +0800

    Bump our `kotlin-common` to the stable version 0.7.0

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -5,7 +5,7 @@ import com.huanshankeji.CommonVersions
 val projectVersion = "0.6.0-SNAPSHOT"
 
 // TODO don't use a snapshot version in a main branch
-val commonVersions = CommonVersions(kotlinCommon = "0.7.0-SNAPSHOT")
+val commonVersions = CommonVersions(kotlinCommon = "0.7.0")
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
 
