0a213968c70b1af874c9c53333cb305aaeb770ea
Yongshun Ye <ShreckYe@gmail.com>
2025-10-31 17:23:04 +0800

    Try making a minimal example to reproduce exceptions caused by multiple `Database`s in multiple threads

--- integrated/src/benchmarks/kotlin/com/huanshankeji/kotlinx/coroutines/benchmark/AbstractRunBlockingAwaitAsyncsBenchmark.kt
+++
@@ -1,7 +0,0 @@
-package com.huanshankeji.kotlinx.coroutines.benchmark
-
-abstract class AbstractRunBlockingAwaitAsyncsBenchmark : AbstractBenchmark() {
-    abstract fun runBlockingAwait1mAsyncs()
-
-    abstract fun runBlockingAwait1KAsync1mSums()
-}
