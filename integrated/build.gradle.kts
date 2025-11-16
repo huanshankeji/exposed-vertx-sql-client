6b71d14e306c03012c265b110ed28bcb02e71bb7
Yongshun Ye <ShreckYe@gmail.com>
2025-11-16 11:23:45 +0800

    Initially try to adapt the `Examples` code into integration tests and extract some common functions
    It seems hard to extract common functions for the database update code because assertions on update counts will be added.

--- integrated/build.gradle.kts
+++ integrated/build.gradle.kts
@@ -12,23 +12,28 @@ dependencies {
 
     implementation(cpnProject(project, ":core"))
     implementation(cpnProject(project, ":postgresql"))
+    implementation(cpnProject(project, ":mysql"))
     implementation(cpnProject(project, ":crud"))
     implementation("com.huanshankeji:exposed-gadt-mapping:${DependencyVersions.exposedGadtMapping}")
     implementation(cpnProject(project, ":crud-with-mapper"))
+
+    with(commonDependencies.testcontainers) {
+        implementation(platformBom())
+        implementation(testcontainersPostgresql)
+        implementation(moduleWithoutVersion("testcontainers-mysql"))
+    }
 }
 
 afterEvaluate {
 // for the benchmarks
     dependencies {
+        // TODO remove `"benchmarksImplementation"`s that are redundant?
+
         with(commonDependencies.vertx) { "benchmarksImplementation"(platformStackDepchain()) } // needed
         // The benchmarks run and "check" passes but the code doesn't resolve without this dependency TODO remove if not needed one day
         "benchmarksImplementation"(cpnProject(project, ":core"))
         "benchmarksImplementation"(cpnProject(project, ":postgresql"))
 
-        with(commonDependencies.testcontainers) {
-            "benchmarksImplementation"(platformBom())
-            "benchmarksImplementation"(testcontainersPostgresql)
-        }
         // Vert.x actually already includes an `slf4j-simple` dependency
         "benchmarksImplementation"(commonDependencies.slf4j.simple())
     }
