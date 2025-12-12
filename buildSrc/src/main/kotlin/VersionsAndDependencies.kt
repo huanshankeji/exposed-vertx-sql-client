1f3c5e7d4388132f575ad8b9a2ec76fdeae9ab03
Yongshun Ye <ShreckYe@gmail.com>
2025-12-12 13:45:01 +0800

    Bump Exposed to 1.0.0-rc-4

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -5,7 +5,7 @@ import com.huanshankeji.CommonVersions
 val projectVersion = "0.7.0-SNAPSHOT"
 
 // don't use a snapshot version in a main branch
-val commonVersions = CommonVersions(kotlinCommon = "0.7.0", exposed = "1.0.0-rc-3")
+val commonVersions = CommonVersions(kotlinCommon = "0.7.0", exposed = "1.0.0-rc-4")
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
 
