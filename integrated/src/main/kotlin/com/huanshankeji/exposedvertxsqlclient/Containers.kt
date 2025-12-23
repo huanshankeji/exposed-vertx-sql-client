huanshankeji/exposed-vertx-sql-client

commit f8ab3ea28b31932aa4fc2ba9cfa5a1c8dbcc0113

Partially add R2DBC transaction benchmarks (mainly `SharedDatabaseTransactionBenchmark`) and connection configuration
Seme code is copied and adapted from the `frameworks/Kotlin/vertx-web-kotlinx` directory of https://github.com/huanshankeji/FrameworkBenchmarks/commit/3c26081ce83a91ab54970f886705d93707102f73.

Rename `setUp` to `setup` to make it consistent with the kotlinx-benchmark annotation BTW.

integrated/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/Containers.kt
@@ -25,23 +25,27 @@ fun JdbcDatabaseContainer<*>.connectionConfig() =
 fun LatestPostgreSQLContainer(): PostgreSQLContainer =
     PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
 
+// TODO consider deprecating these functions and recommending the caller to call `connectionConfig()` and then call the extensions function on `ConnectionConfig.Socket` instead
 fun PostgreSQLContainer.exposedDatabaseConnect(): Database =
     connectionConfig().exposedDatabaseConnectPostgresql()
 
 // move to the `postgresql` module if it's proved useful
-fun PostgreSQLContainer.hikariConfig(maximumPoolSize: Int, extraConfig: HikariConfig.() -> Unit = {}): HikariConfig {
-    val connectionConfig = connectionConfig()
-    return HikariConfig().apply {
-        jdbcUrl = connectionConfig.postgresqlJdbcUrl()
+fun ConnectionConfig.Socket.hikariConfig(maximumPoolSize: Int, extraConfig: HikariConfig.() -> Unit): HikariConfig =
+    HikariConfig().apply {
+        jdbcUrl = postgresqlJdbcUrl()
         driverClassName = "org.postgresql.Driver" // TODO extract
-        username = connectionConfig.user
-        password = connectionConfig.password
+        username = user
+        this.password = password
         this.maximumPoolSize = maximumPoolSize
         // configurations for SQL preparation for Vert.x SQL Clients
         isReadOnly = true
         transactionIsolation = "TRANSACTION_READ_UNCOMMITTED"
         extraConfig()
     }
+
+fun PostgreSQLContainer.hikariConfig(maximumPoolSize: Int, extraConfig: HikariConfig.() -> Unit = {}): HikariConfig {
+    val connectionConfig = connectionConfig()
+    return connectionConfig.hikariConfig(maximumPoolSize, extraConfig)
 }
 
 fun PostgreSQLContainer.hikariDataSource(maximumPoolSize: Int, extraConfig: HikariConfig.() -> Unit = {}) =

gradle/libs.versions.toml
@@ -1,0 +1,12 @@
+[versions]
+r2dbc-spi = "1.0.0.RELEASE"
+r2dbc-postgresql = "1.1.1.RELEASE"
+r2dbc-pool = "1.0.2.RELEASE"
+exposed = "1.0.0-rc-4"
+
+[libraries]
+r2dbc-spi = { module = "io.r2dbc:r2dbc-spi", version.ref = "r2dbc-spi" }
+r2dbc-postgresql = { module = "org.postgresql:r2dbc-postgresql", version.ref = "r2dbc-postgresql" }
+r2dbc-pool = { module = "io.r2dbc:r2dbc-pool", version.ref = "r2dbc-pool" }
+
+exposed-r2dbc = { module = "org.jetbrains.exposed:exposed-r2dbc", version.ref = "exposed" }

integrated/build.gradle.kts
@@ -41,6 +41,11 @@ dependencies {
     testRuntimeOnly(commonDependencies.slf4j.simple())
 
     implementation("com.zaxxer:HikariCP:${DependencyVersions.hikaricp}")
+
+    implementation(libs.r2dbc.spi)
+    implementation(libs.r2dbc.postgresql)
+    implementation(libs.r2dbc.pool)
+    implementation(libs.exposed.r2dbc)
 }
 
 // to resolve "no tests discovered" errors when running `check`

integrated/src/benchmarks/kotlin/com/huanshankeji/exposed/benchmark/ConstantsAndHelpers.kt
@@ -1,5 +1,8 @@
 package com.huanshankeji.exposed.benchmark
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlin.concurrent.thread
 
 val numProcessors = Runtime.getRuntime().availableProcessors()
@@ -26,3 +29,9 @@ inline fun multiThread_10K_nearlyEvenlyPartitioned_helper(
     crossinline threadBlock: (num: Int) -> Unit
 ) =
     multiThread_operations_nearlyEvenlyPartitioned_helper(`10K`, numThreads, threadBlock)
+
+// ! `await` is actually quite expensive.
+@Suppress("SuspendFunctionOnCoroutineScope")
+suspend inline fun CoroutineScope.awaitAsync10K(crossinline block: suspend () -> Unit) =
+    //List(`10K`) { async { block() } }.awaitAll()
+    awaitAll(*Array(`10K`) { async { block() } })

integrated/src/benchmarks/kotlin/com/huanshankeji/exposed/benchmark/TransactionBenchmark.kt
@@ -60,11 +60,6 @@ class TransactionBenchmark : WithContainerizedDatabaseAndExposedDatabaseBenchmar
         repeat(`10K`) { suspendTransaction(database) {} }
     }
 
-    // ! `await` is actually quite expensive.
-    @Suppress("SuspendFunctionOnCoroutineScope")
-    private suspend inline fun CoroutineScope.awaitAsync10K(crossinline block: () -> Unit) =
-        List(`10K`) { async { block() } }.awaitAll()
-
     /**
      * For debugging purposes.
      */

integrated/src/benchmarks/kotlin/com/huanshankeji/exposed/benchmark/WithContainerizedDatabaseAndExposedDatabaseBenchmark.kt
@@ -10,8 +10,8 @@ abstract class WithContainerizedDatabaseAndExposedDatabaseBenchmark : WithContai
         postgreSQLContainer.exposedDatabaseConnect()
 
     @Setup
-    override fun setUp() {
-        super.setUp()
+    override fun setup() {
+        super.setup()
         database = databaseConnect()
     }
 }

integrated/src/benchmarks/kotlin/com/huanshankeji/exposed/benchmark/WithContainerizedDatabaseAndHikaricpExposedDatabaseBenchmark.kt
@@ -17,8 +17,8 @@ abstract class WithContainerizedDatabaseAndHikaricpExposedDatabaseBenchmark : Wi
     }
 
     @Setup
-    override fun setUp() {
-        super.setUp()
+    override fun setup() {
+        super.setup()
         hikariDataSource = postgreSQLContainer.hikariDataSource(maximumPoolSize)
         database = databaseConnect()
     }

integrated/src/benchmarks/kotlin/com/huanshankeji/exposed/benchmark/WithContainerizedDatabaseBenchmark.kt
@@ -1,14 +1,16 @@
 package com.huanshankeji.exposed.benchmark
 
 import com.huanshankeji.exposedvertxsqlclient.LatestPostgreSQLContainer
+import com.huanshankeji.exposedvertxsqlclient.connectionConfig
 import kotlinx.benchmark.Setup
 import kotlinx.benchmark.TearDown
 
 abstract class WithContainerizedDatabaseBenchmark : AbstractBenchmark() {
     val postgreSQLContainer = LatestPostgreSQLContainer()
+    val connectionConfig get() = postgreSQLContainer.connectionConfig()
 
     @Setup
-    fun setUp() {
+    fun setup() {
         postgreSQLContainer.start()
     }
 

commit 2e9d11b0ea7c97ae184551cfa1282e0ee0ee7eb1

Fix a bug introduced in f8ab3ea28b31932aa4fc2ba9cfa5a1c8dbcc0113

integrated/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/Containers.kt
@@ -35,7 +35,7 @@ fun ConnectionConfig.Socket.hikariConfig(maximumPoolSize: Int, extraConfig: Hika
         jdbcUrl = postgresqlJdbcUrl()
         driverClassName = "org.postgresql.Driver" // TODO extract
         username = user
-        this.password = password
+        password = this@hikariConfig.password
         this.maximumPoolSize = maximumPoolSize
         // configurations for SQL preparation for Vert.x SQL Clients
         isReadOnly = true