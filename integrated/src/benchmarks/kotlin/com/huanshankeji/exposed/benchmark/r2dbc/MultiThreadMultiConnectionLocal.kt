d35d484c6757136652a4ed63d0da25327a0a0b5c
Yongshun Ye <ShreckYe@gmail.com>
2025-12-23 20:46:09 +0800

    Benchmark with 1 million transactions as a unit to reduce the overhead in `MultiThreadMultiConnectionLocalTransactionBenchmark`

--- integrated/src/benchmarks/kotlin/com/huanshankeji/exposed/benchmark/r2dbc/MultiThreadMultiConnectionLocal.kt
+++ integrated/src/benchmarks/kotlin/com/huanshankeji/exposed/benchmark/r2dbc/MultiThreadMultiConnectionLocal.kt
@@ -1,30 +1,43 @@
 package com.huanshankeji.exposed.benchmark.r2dbc
 
+import com.huanshankeji.exposed.benchmark.`1M`
 import com.huanshankeji.exposed.benchmark.WithContainerizedDatabaseBenchmark
-import com.huanshankeji.exposed.benchmark.multiThread_10K_ops_nearlyEvenlyPartitioned_helper
+import com.huanshankeji.exposed.benchmark.multiThread_ops_nearlyEvenlyPartitioned_helper
 import com.huanshankeji.exposed.benchmark.numProcessors
 import com.huanshankeji.exposedvertxsqlclient.r2dbc.exposedR2dbcDatabaseConnectPostgresql
 import io.r2dbc.spi.IsolationLevel
 import kotlinx.benchmark.Benchmark
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
 import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 
 class MultiThreadMultiConnectionLocalTransactionBenchmark : WithContainerizedDatabaseBenchmark() {
-    // TODO try `1M`
-    private inline fun multiThread_multiConnection_inTotal_10K_runBlocking_local_suspendTransactions_nearlyEvenlyPartitioned_helper(
+    private inline fun multiThread_multiConnection_inTotal_1M_runBlocking_local_suspendTransactions_nearlyEvenlyPartitioned_helper(
         numThreads: Int = numProcessors,
-        crossinline transactionBlock: suspend (database: R2dbcDatabase) -> Unit
+        crossinline numTransactionBlock: suspend (database: R2dbcDatabase, num: Int) -> Unit
     ) {
-        multiThread_10K_ops_nearlyEvenlyPartitioned_helper(numThreads) { num ->
+        multiThread_ops_nearlyEvenlyPartitioned_helper(`1M`, numThreads) { num ->
             val database = connectionConfig.exposedR2dbcDatabaseConnectPostgresql()
-            runBlocking { repeat(num) { transactionBlock(database) } }
+            runBlocking { numTransactionBlock(database, num) }
         }
     }
 
     @Benchmark
-    fun mt_mc_inTotal_10K_rb_local_ru_ro_suspendTransactions_nep() =
-        multiThread_multiConnection_inTotal_10K_runBlocking_local_suspendTransactions_nearlyEvenlyPartitioned_helper {
-            suspendTransaction(it, IsolationLevel.READ_UNCOMMITTED, true) {}
+    fun mt_mc_inTotal_1M_runBlocking_local_ru_ro_suspendTransactions_nep() =
+        multiThread_multiConnection_inTotal_1M_runBlocking_local_suspendTransactions_nearlyEvenlyPartitioned_helper { database, num ->
+            repeat(num) { suspendTransaction(database, IsolationLevel.READ_UNCOMMITTED, true) {} }
+        }
+
+    @Benchmark
+    fun mt_mc_inTotal_1M_await_async_local_ru_ro_suspendTransactions_nep() =
+        multiThread_multiConnection_inTotal_1M_runBlocking_local_suspendTransactions_nearlyEvenlyPartitioned_helper { database, num ->
+            coroutineScope {
+                awaitAll(*Array(num) {
+                    async { suspendTransaction(database, IsolationLevel.READ_UNCOMMITTED, true) {} }
+                })
+            }
         }
 }
