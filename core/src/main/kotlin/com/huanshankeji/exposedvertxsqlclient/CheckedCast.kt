f89f750fd48a5791cac02d1d1d8a20598a6ea082
Yongshun Ye <shreckye@gmail.com>
2026-01-06 16:31:59 +0800

    Mark the 2 added cast APIs as experiment and internal

--- core/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/CheckedCast.kt
+++ core/src/main/kotlin/com/huanshankeji/exposedvertxsqlclient/CheckedCast.kt
@@ -2,6 +2,8 @@ package com.huanshankeji.exposedvertxsqlclient
 
 import io.vertx.sqlclient.SqlClient
 
+@ExperimentalEvscApi
+@InternalApi
 inline fun <reified T : SqlClient> DatabaseClient<*>.withVertxSqlClientCheckedCastToOrNull(): DatabaseClient<T>? =
     if (vertxSqlClient is T)
         @Suppress("UNCHECKED_CAST")
@@ -9,6 +11,8 @@ inline fun <reified T : SqlClient> DatabaseClient<*>.withVertxSqlClientCheckedCa
     else
         null
 
+@ExperimentalEvscApi
+@InternalApi
 inline fun <reified T : SqlClient> DatabaseClient<*>.withVertxSqlClientCheckedCastTo(): DatabaseClient<T> =
     withVertxSqlClientCheckedCastToOrNull()
         ?: throw ClassCastException("cannot cast DatabaseClient<${vertxSqlClient::class.simpleName}> to DatabaseClient<${T::class.simpleName}>")
