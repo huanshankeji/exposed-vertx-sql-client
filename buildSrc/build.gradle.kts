9f30629e0b26c2301e5202319e225edf51c0f189
Yongshun Ye <ShreckYe@gmail.com>
2026-07-24 09:26:33 +0800

    Review all PR changes and make final improvements

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,6 @@ dependencies {
     implementation(kotlin("gradle-plugin", "2.4.0"))
     implementation("com.huanshankeji:common-gradle-dependencies:0.10.0-20251024")
     implementation("com.huanshankeji.team:project-gradle-plugins:$gradleCommonPluginsVersion")
-    implementation("com.huanshankeji:kotlin-common-project-gradle-plugins:$gradleCommonPluginsVersion")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.2.0")
 }
 
